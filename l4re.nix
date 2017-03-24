7bbac557ec75f41d11d3603502854ab26b81ff23
Mateusz Czaplinski <czapkofan@gmail.com>
2017-03-24 17:29:53 +0000

    l4re: successful download

--- l4re.nix
+++ l4re.nix
@@ -1,9 +1,12 @@
-{ stdenv, fetchurl }:
+#{ stdenv, fetchurl }:      # nixpkgs .nix
+with import <nixpkgs> {};  # standalone .nix
+
+# INFO: run with `nix-build $FILE.nix`
 
 stdenv.mkDerivation {
   name = "l4re-0.0.1";  # FIXME(akavel): fix version
   src = fetchurl {
     url = http://os.inf.tu-dresden.de/download/snapshots/l4re-core-2016082114.tar.xz;  # FIXME(akavel): use SVN; snapshot may become stale
-    sha256 = "1193da7c4838a829db978612323877d4926b9ae21487d3f28b937ef929650faa";
+    sha256 = "cd50e7f3c2c0bc7d62db23790f3ad856694defe5b2da8d95ca9f34a051937f88";
   };
 }
