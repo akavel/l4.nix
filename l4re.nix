c6d5955c9e3867f572127657b861b9291dd63e2a
Mateusz Czaplinski <czapkofan@gmail.com>
2017-03-24 17:45:17 +0000

    l4re: unpack

--- l4re.nix
+++ l4re.nix
@@ -6,7 +6,21 @@ with import <nixpkgs> {};  # standalone .nix
 stdenv.mkDerivation {
   name = "l4re-0.0.1";  # FIXME(akavel): fix version
   src = fetchurl {
-    url = http://os.inf.tu-dresden.de/download/snapshots/l4re-core-2016082114.tar.xz;  # FIXME(akavel): use SVN; snapshot may become stale
+    # see: http://os.inf.tu-dresden.de/L4Re/download.html
+    # FIXME(akavel): use SVN; snapshot may become stale
+    url = http://os.inf.tu-dresden.de/download/snapshots/l4re-core-2016082114.tar.xz;
     sha256 = "cd50e7f3c2c0bc7d62db23790f3ad856694defe5b2da8d95ca9f34a051937f88";
   };
+  # TODO(akavel): make sure we build for x86 / x64 / whatever we need
+  # (should be configurable via Nix, with host arch autoconfigured by default on NixOS)
+  # see: http://os.inf.tu-dresden.de/L4Re/build.html
+  builder = builtins.toFile "builder.sh" ''
+    source $stdenv/setup
+
+    tar xJf $src
+    cd l4re-core-*/src/l4
+    make B=$out
+    make O=$out config
+    make O=$out
+  '';
 }
