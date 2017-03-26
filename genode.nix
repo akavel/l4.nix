e9092656ce7be8126631bfcda0be2267fe1810bd
Mateusz Czaplinski <czapkofan@gmail.com>
2017-03-26 09:39:56 +0000

    genode: unpack the toolchain

--- genode.nix
+++ genode.nix
@@ -7,12 +7,22 @@ with import <nixpkgs> {};  # standalone .nix
 
 let
   # TODO(akavel): add option to build from source instead
-  genode-toolchain-bin = stdenv.mkDerivation {
+  genode-toolchain-bin = stdenv.mkDerivation rec {
     name = "genode-toolchain-16.05-x86_64-bin";
     src = fetchurl {
       url = mirror://sourceforge/genode/genode-toolchain/16.05/genode-toolchain-16.05-x86_64.tar.bz2;
       sha256 = "07k41p2ssr6vq793g766y5ng14ljx9x5d5qy2zvjkq7csqr9hr1j";
     };
+    builder = builtins.toFile "builder.sh" ''
+      source $stdenv/setup
+
+      echo unpacking...
+      mkdir -p $out
+      tar -C $out -xf $src /usr/local/genode-gcc --strip-components=4
+
+      echo fixup...
+      fixupPhase
+    '';
   };
 in
   genode-toolchain-bin
