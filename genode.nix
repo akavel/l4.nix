0e02ec8153883268a5392c9db5ea047cfcd85f64
Mateusz Czaplinski <czapkofan@gmail.com>
2017-03-26 09:03:11 +0000

    genode: WIP initial download of toolchain

---
+++ genode.nix
@@ -0,0 +1,18 @@
+#{ stdenv, fetchurl }:      # nixpkgs .nix
+with import <nixpkgs> {};  # standalone .nix
+
+# INFO: run with `nix-build $FILE.nix`
+# INFO: for easier debugging, `nix-build -K $FILE.nix` - this keeps failed results in /tmp/nix-...
+#  - see Nix manual 14.4.1
+
+let
+  # TODO(akavel): add option to build from source instead
+  genode-toolchain-bin = stdenv.mkDerivation {
+    name = "genode-toolchain-16.05-x86_64-bin";
+    src = fetchurl {
+      url = mirror://sourceforge/genode/genode-toolchain/16.05/genode-toolchain-16.05-x86_64.tar.bz2;
+      sha256 = "07k41p2ssr6vq793g766y5ng14ljx9x5d5qy2zvjkq7csqr9hr1j";
+    };
+  };
+in
+  genode-toolchain-bin
