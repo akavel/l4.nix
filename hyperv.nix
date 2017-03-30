fe4b888849036472e42faa9136bd4ecac2320a4b
Mateusz Czaplinski <czapkofan@gmail.com>
2017-03-30 18:10:48 +0000

    hyperv: build & install seems successful

--- hyperv.nix
+++ hyperv.nix
@@ -18,11 +18,21 @@ let
   # Based on <nixpkgs>/pkgs/os-specific/linux/kernel/perf.nix
   hv_daemons = stdenv.mkDerivation {
     name = "hv_daemons-${kernel.version}";
-    inherit (kernel) src patches;
+    inherit (kernel) src;
+    patches = kernel.patches ++ [
+      #./tools-hv-4.4-4.7.patch
+      ./tools-hv-4.7-4.10.patch
+    ];
+    # FIXME(akavel): workaround for error fixed in 4.10
+    hardeningDisable = [ "fortify "];
     preConfigure = ''
       cd tools/hv
-      #sed -i s,/usr/include/elfutils,$elfutils/include/elfutils, Makefile
-      export makeFlags="DESTDIR=$out $makeFlags"
+    '';
+    installPhase = ''
+      mkdir -p $out
+      for f in lsvmbus $(find . -type f -executable); do
+        mv $f $out
+      done
     '';
   };
 in {
